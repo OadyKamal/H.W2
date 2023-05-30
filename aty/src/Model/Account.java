3f8ec6c5f8eccbb1733aeb786b1c584f446c384a
OadyKamal <118626608+OadyKamal@users.noreply.github.com>
2023-05-30 18:40:01 +0300

    Account.java

--- aty/src/Model/Account.java
+++ aty/src/Model/Account.java
@@ -1,6 +1,5 @@
 
 package Model;
-
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -8,21 +7,20 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class Account {
-
     private int id;
-    private int user_id;
-    private int account_number;
+    private int userId;
+    private int accountNumber;
     private String username;
     private String currency;
-   private double balance;
-      private int creation_date;
+    private double balance;
+    private int creationDate;
 
-    public Account(int account_number, String username, String currency, double balance, int creation_date) {
-        this.account_number = account_number;
+    public Account(int accountNumber, String username, String currency, double balance, int creationDate) {
+        this.accountNumber = accountNumber;
         this.username = username;
         this.currency = currency;
         this.balance = balance;
-        this.creation_date = creation_date;
+        this.creationDate = creationDate;
     }
 
     public int getId() {
@@ -33,20 +31,20 @@ public class Account {
         this.id = id;
     }
 
-    public int getUser_id() {
-        return user_id;
+    public int getUserId() {
+        return userId;
     }
 
-    public void setUser_id(int user_id) {
-        this.user_id = user_id;
+    public void setUserId(int userId) {
+        this.userId = userId;
     }
 
-    public int getAccount_number() {
-        return account_number;
+    public int getAccountNumber() {
+        return accountNumber;
     }
 
-    public void setAccount_number(int account_number) {
-        this.account_number = account_number;
+    public void setAccountNumber(int accountNumber) {
+        this.accountNumber = accountNumber;
     }
 
     public String getUsername() {
@@ -73,32 +71,36 @@ public class Account {
         this.balance = balance;
     }
 
-    public int getCreation_date() {
-        return creation_date;
+    public int getCreationDate() {
+        return creationDate;
     }
 
-    public void setCreation_date(int creation_date) {
-        this.creation_date = creation_date;
+    public void setCreationDate(int creationDate) {
+        this.creationDate = creationDate;
     }
 
     public int save() throws SQLException, ClassNotFoundException {
         Connection c = DB.getInstance().getConnection();
         PreparedStatement ps = null;
         int recordCounter = 0;
-        String sql = "INSERT INTO ACCOUNT(ID,USER_ID,ACCOUNT_NUMBER,USERNAME,CURRENCY,BALANCE,CREATION_DATE) VALUES(?, ?, ?, ?,?,?,?)";
-        ps = c.prepareStatement(sql);
-        ps.setInt(1, this.getId());
-        ps.setInt(2, this.getUser_id());
-        ps.setInt(3, this.getAccount_number());
-        ps.setString(4, this.getUsername());
-        ps.setString(5, this.getCurrency());
-        ps.setDouble(6, this.getBalance());
-        ps.setInt(7, this.getCreation_date());
+        String sql = "INSERT INTO ACCOUNT(USER_ID, ACCOUNT_NUMBER, USERNAME, CURRENCY, BALANCE, CREATION_DATE) VALUES (?, ?, ?, ?, ?, ?)";
+        ps = c.prepareStatement(sql, PreparedStatement.RETURN_GENERATED_KEYS);
+        ps.setInt(1, this.getUserId());
+        ps.setInt(2, this.getAccountNumber());
+        ps.setString(3, this.getUsername());
+        ps.setString(4, this.getCurrency());
+        ps.setDouble(5, this.getBalance());
+        ps.setInt(6, this.getCreationDate());
         recordCounter = ps.executeUpdate();
+
         if (recordCounter > 0) {
-            System.out.println(this.getUsername()
-                    + " Accounts was added successfully!");
+            ResultSet generatedKeys = ps.getGeneratedKeys();
+            if (generatedKeys.next()) {
+                this.setId(generatedKeys.getInt(1));
+            }
+            System.out.println(this.getUsername() + "'s account was added successfully!");
         }
+
         if (ps != null) {
             ps.close();
         }
@@ -106,80 +108,70 @@ public class Account {
         return recordCounter;
     }
 
-    
-    public static ArrayList<Account> getAllAccount() throws SQLException, ClassNotFoundException {
+    public static ArrayList<Account> getAllAccounts() throws SQLException, ClassNotFoundException {
         Connection c = DB.getInstance().getConnection();
         PreparedStatement ps = null;
         ResultSet rs = null;
         ArrayList<Account> accounts = new ArrayList<>();
-        String sql = "SELECT * FROM ACCOUNT ";
+        String sql = "SELECT * FROM ACCOUNT";
         ps = c.prepareStatement(sql);
         rs = ps.executeQuery();
-        while (rs.next()) {
-            Account account = new Account(rs.getInt(2), rs.getString(3), rs.getString(4), rs.getDouble(5), rs.getInt(6));
-            account.setId(rs.getInt(1));
-            accounts.add(account);
 
+        while (rs.next()) {
+            Account account = new Account(rs.getInt("ACCOUNT_NUMBER"), rs.getString("USERNAME"), rs.getString("CURRENCY"), rs.getDouble("BALANCE"), rs.getInt("CREATION_DATE"));
+            account.setId(rs.getInt("ID"));
+            accounts.add(account);
         }
+
         if (ps != null) {
             ps.close();
         }
         c.close();
         return accounts;
     }
-  public int delete() throws SQLException, ClassNotFoundException {
+
+    public int update() throws SQLException, ClassNotFoundException {
         Connection c = DB.getInstance().getConnection();
         PreparedStatement ps = null;
         int recordCounter = 0;
-        String sql = "DELETE FROM ACCOUNT WHERE ID=? ";
+        String sql = "UPDATE ACCOUNT SET ACCOUNT_NUMBER=?, USERNAME=?, CURRENCY=?, BALANCE=?, CREATION_DATE=? WHERE ID=?";
         ps = c.prepareStatement(sql);
-        ps.setInt(1, this.getId());
+        ps.setInt(1, this.getAccountNumber());
+        ps.setString(2, this.getUsername());
+        ps.setString(3, this.getCurrency());
+        ps.setDouble(4, this.getBalance());
+        ps.setInt(5, this.getCreationDate());
+        ps.setInt(6, this.getId());
         recordCounter = ps.executeUpdate();
+
         if (recordCounter > 0) {
-            System.out.println("The account with id : " + this.getId() + " was deleted successfully!");
+            System.out.println("Account with ID: " + this.getId() + " was updated successfully!");
         }
+
         if (ps != null) {
             ps.close();
         }
         c.close();
         return recordCounter;
     }
-   
-  
-  
-  
-    public int update() throws SQLException, ClassNotFoundException {
-           Connection c = DB.getInstance().getConnection();
-     PreparedStatement ps = null;
-            int recordCounter = 0;
-      String sql = "UPDATE ACCOUNT ACCOUNT_NUMBER=?, USERNAME=? , CURRENCY=?,BALANCE=? ,CREATION_DATE=? WHERE ID=?";
-          ps = c.prepareStatement(sql);
-         ps.setInt(1, this.getAccount_number());
-       ps.setString(2, this.getUsername());
-         ps.setString(3, this.getCurrency());
-     ps.setDouble(4, this.getBalance());
-ps.setInt(5, this.getCreation_date());
-ps.setInt(6, this.getId());
-   recordCounter = ps.executeUpdate();
-       if (recordCounter > 0) {
-         
-           System.out.println("Account with id : " + this.getId() + " was updated successfully!");
-       
-       
-       
-       }
-       if (ps != null) {
+
+    public int delete() throws SQLException, ClassNotFoundException {
+        Connection c = DB.getInstance().getConnection();
+        PreparedStatement ps = null;
+        int recordCounter = 0;
+        String sql = "DELETE FROM ACCOUNT WHERE ID=?";
+        ps = c.prepareStatement(sql);
+        ps.setInt(1, this.getId());
+        recordCounter = ps.executeUpdate();
+
+        if (recordCounter > 0) {
+            System.out.println("The account with ID: " + this.getId() + " was deleted successfully!");
+        }
+
+        if (ps != null) {
             ps.close();
-       
-        
-        
         }
         c.close();
-      
-        
         return recordCounter;
     }
-
-  
-
 }
